987601ab20b692e3d6dcbbb4932cbfb8fbdc19a9
namhyeop <skaguq@gmail.com>
2022-11-18 12:42:43 +0900

    Feat : Calculate Config 오류 수정

--- src/main/java/shop/kokodo/calculateservice/CalculateServiceApplication.java
+++ src/main/java/shop/kokodo/calculateservice/CalculateServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableFeignClients
