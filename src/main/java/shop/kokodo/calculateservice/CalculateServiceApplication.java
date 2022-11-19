af9d243b45b3770b97d8ff218bf3ae4b87a4c84d
namhyeop <skaguq@gmail.com>
2022-11-19 21:31:13 +0900

    Feat : Spring Batch, DBConfig 충동 오류 수정

--- src/main/java/shop/kokodo/calculateservice/CalculateServiceApplication.java
+++ src/main/java/shop/kokodo/calculateservice/CalculateServiceApplication.java
@@ -1,11 +1,9 @@
 package shop.kokodo.calculateservice;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -15,5 +13,4 @@ public class CalculateServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CalculateServiceApplication.class, args);
     }
-
 }
